260d0396161731478de9d610dd318b14ed1c7299
Onyedika Benjamin <benbillion62@gmail.com>
2024-12-25 23:52:15 +0100

    updated

--- main.go
+++ main.go
@@ -3,15 +3,33 @@ package main
 import (
 	"ecommerce-api/routes"
 	"ecommerce-api/models"
+	"github.com/joho/godotenv"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
 	"log"
+	"os"
+	"fmt"
 )
 
 var db *gorm.DB
 
 func init() {
-	dsn := "root:black0111@tcp(localhost:3306)/ecommerce?charset=utf8mb4&parseTime=True&loc=Local"
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Error loading .env file: %v", err)
+	}
+
+	dbUser := os.Getenv("DB_USER")
+	dbPassword := os.Getenv("DB_PASSWORD")
+	dbHost := os.Getenv("DB_HOST")
+	dbPort := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
+	dbCharset := os.Getenv("DB_CHARSET")
+	dbParseTime := os.Getenv("DB_PARSE_TIME")
+	dbLoc := os.Getenv("DB_LOC")
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=%s&parseTime=%s&loc=%s",
+		dbUser, dbPassword, dbHost, dbPort, dbName, dbCharset, dbParseTime, dbLoc)
+
 	var err error
 	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
 	if err != nil {
@@ -25,9 +43,45 @@ func init() {
 }
 
 func main() {
+	serverPort := os.Getenv("SERVER_PORT")
+
 	r := routes.SetupRouter(db)
 
-	if err := r.Run(":8080"); err != nil {
+	if err := r.Run(fmt.Sprintf(":%s", serverPort)); err != nil {
 		log.Fatalf("Server failed to start: %v", err)
 	}
 }
+
+// package main
+
+// import (
+// 	"ecommerce-api/routes"
+// 	"ecommerce-api/models"
+// 	"gorm.io/driver/mysql"
+// 	"gorm.io/gorm"
+// 	"log"
+// )
+
+// var db *gorm.DB
+
+// func init() {
+// 	dsn := "root:black0111@tcp(localhost:3306)/ecommerce?charset=utf8mb4&parseTime=True&loc=Local"
+// 	var err error
+// 	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+// 	if err != nil {
+// 		log.Fatalf("Could not connect to the database: %v", err)
+// 	}
+
+// 	err = db.AutoMigrate(&models.User{}, &models.Product{}, &models.Order{}, &models.OrderItem{})
+// 	if err != nil {
+// 		log.Fatalf("Could not migrate database: %v", err)
+// 	}
+// }
+
+// func main() {
+// 	r := routes.SetupRouter(db)
+
+// 	if err := r.Run(":8080"); err != nil {
+// 		log.Fatalf("Server failed to start: %v", err)
+// 	}
+// }
